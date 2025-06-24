11c31ec93e7f4d2977eaf1f66921acc426bb1507
Eder Ortega <eder.ortega@ceimic.cloud>
2025-06-23 22:47:52 -0300

    EOF - Cambio en el tamaño del logo del Email de recuperar contraseña

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -7,8 +7,10 @@
                 El slot (o el logo por defecto de Laravel) se reemplaza por tu logo personalizado.
                 El valor de $url lo toma de la variable APP_URL en tu .env
             --}}
-            <img src="{{ asset('images/logo_ceimic.png') }}" class="logo" alt="{{ config('app.name', 'Laravel') }} Logo"
-                style="max-width: 220px; height: auto; border: none;">
+            <img src="{{ asset('images/logo_ceimic.png') }}" 
+                width="200" 
+                alt="{{ config('app.name', 'Laravel') }} Logo"
+                style="display: block; width: 200px; height: auto; border: none;">
         </a>
     </td>
 </tr>
